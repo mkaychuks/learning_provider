865612c5ee3b49133a28d071387b67e83766c105
mkaychuks <sachufusi@gmail.com>
2023-04-24 15:17:22 +0100

    adding my assets

--- lib/provider/weather_provider.dart
+++ lib/provider/weather_provider.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:learning_provider/model/weather.dart';
+import 'package:learning_provider/utils/commons.dart';
 
 class WeatherProvider extends ChangeNotifier {
   double _latitude = 0.0;
   double _longitude = 0.0;
   String _city = '';
+  Weather? _weather;
 
   double get latitude => _latitude;
   double get longitude => _longitude;
   String get city => _city;
-
+  dynamic get weather => _weather;
 
   /// a function we call to fetch our longitude and latitude
   Future getUserLocation() async {
     bool isserviceEnabled; // the location service availability.
     LocationPermission permission; // the nature of permission granted..
 
-
     // checking the service availability.
-    isserviceEnabled = await Geolocator.isLocationServiceEnabled(); 
+    isserviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!isserviceEnabled) {
       return Future.error("Location services are disabled");
     }
@@ -45,23 +48,41 @@ class WeatherProvider extends ChangeNotifier {
         /// now calling the getAddress() function because
         /// it depends on the current value of the latitude and longitude..
         getAddress(value.latitude, value.longitude);
+        fetchWeatherDataFromAPI(value.latitude, value.longitude);
         notifyListeners();
         print("$_latitude :::::::::::$_longitude");
       },
     );
   }
 
-
   /// get the address from the latitude and longitude already
   /// gotten from our previous function.
   getAddress(lat, lon) async {
     // calling the geocoding dependency to help us with that
     List<Placemark> placemark = await placemarkFromCoordinates(lat, lon);
+
     /// the result is a list of objects, so we need to access the 0 index
     /// and store in a variable "currentAddress"
     var currentAddress = placemark[0].locality;
     // modify the state of our provider to the new state of the city
     _city = currentAddress!;
     notifyListeners(); // notifying the listeners.
-  } 
+  }
+
+  /// the function that will call the WeatherAPI
+  Future fetchWeatherDataFromAPI(a,b) async {
+    var uri =
+        "https://api.weatherbit.io/v2.0/current?lat=$a&lon=$b&key=$apiKey";
+
+    try {
+      http.Response response = await http.get(Uri.parse(uri));
+      if (response.statusCode == 200) {
+        _weather = weatherFromJson(response.body);
+        print("::::::::::::::::: ${_weather!}");
+        notifyListeners();
+      }
+    } catch (e) {
+      print("::::::::::::::\$ERROR MESSAGE IS THIS ${e.toString()}");
+    }
+  }
 }
